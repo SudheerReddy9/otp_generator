35431c6c9741935c021dec3f20305c1df0f7d929
SudheerReddy9 <pokalasudheer143@gmail.com>
2024-02-24 21:35:30 -0500

    go

---
+++ emailotp.js
@@ -0,0 +1,35 @@
+function sendOTP(){
+    const email = document.getElementById('email');
+    const otpverify = document.getElementsByClassName('otpverify')[0];
+
+    let otp_val = Math.floor(Math.random() * 10000);
+
+    let emailbody = `<h2> Your OTP is${otp_val}`;
+
+    Email.send({
+    SecureToken : "76984367-5f5e-45cd-93c6-7e9bca0053d3",
+    To : email.vale,
+    From : "sudheerreddypokala@gmail.com",
+    Subject : "Email OTP using JavaScript",
+    Body : emailbody,
+    }).then(
+      message =>{
+        if (message === 'OK'){
+            alert("OTP send to your email" + email.value);
+
+            otpverify.style.display = "flex";
+            const otp_inp = document.getElementById("otp_inp");
+            const otp_btn = document.getElementById('otp-btn');
+
+            otp_btn.addEventListener('click',() => {
+                if(otp_inp.value == otp_val){
+                    alert("Email address verified....");
+                } 
+                else{
+                    alert("Invalid OTP");
+                }
+            })
+        }
+      }
+    );
+}
